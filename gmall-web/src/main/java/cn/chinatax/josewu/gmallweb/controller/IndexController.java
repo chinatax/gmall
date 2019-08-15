341e9d0af23a7dc42c77b4b18b2aec3f5ec9b96e
chinatax <446702119@qq.com>
2019-08-15 22:09:41 +0800

    建立springboot工程gmall后，首次提交; 增加了几个springboot子工程:gmall-base                          gmall-dao                          gmall-service                          gmall-web 增加各个子模块之间的依赖关系，删除各子模块不需要的文件夹 index 测试页面

--- gmall-web/src/main/java/cn/chinatax/josewu/gmallweb/controller/IndexController.java
+++ gmall-web/src/main/java/cn/chinatax/josewu/gmallweb/controller/IndexController.java
@@ -1,9 +1,11 @@
 package cn.chinatax.josewu.gmallweb.controller;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
-
+import cn.chinatax.josewu.base.bean.User;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,14 +14,39 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/index")
 public class IndexController {
 
+
+	@Value(value= "${random.value}")
+	private String secret;
+
+
 	@RequestMapping
 	public String index() {
 
 		return "hello world!";
 
 	}
-	
+	@RequestMapping(value = "get")
+	public Map<String, Object> get(@RequestParam String name) {
 
+		Map<String, Object> map = new HashMap<String, Object>();
+
+		map.put("name", name);
+		map.put("value", "hello world!");
+		map.put("secret", secret);
+		return map;
+
+	}
+	@RequestMapping(value = "find/{id}/{name}")
+	public User get(@PathVariable int id, @PathVariable String name) {
+
+		User u = new User();
+		u.setId(id);
+		u.setName(name);
+		u.setDate(new Date());
+
+		return u;
+
+	}
 
 
 }
